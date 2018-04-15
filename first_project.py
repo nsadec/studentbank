649459b81634b98c8b445d64bdc361b839836a27
nsadec <37998566+nsadec@users.noreply.github.com>
2018-04-16 01:19:34 +0300

    Add open source and Software engineer materials

--- first_project.py
+++ first_project.py
@@ -100,8 +100,21 @@ def handle(msg):
             db.insert_date(conn, 'bot', "send, mass, type,user,id,time",
                            "'{}','{}','{}','{}', '{}','{}'".format(Suggestion, a, Suggestion, chat_username, chat_id,
                                                                    tim))
-    elif command == "547" or command == "IT 547" or command == "IT 547" or command == "Open Source Technology":
-        a = "زودني باسم المادة"
+    elif command == "وجهني":
+        a = "اكتب اسم المقرر الذي تحتاج مصادره"\
+            "تقنيات المصادر المفتوحة"\
+            "هندسة البرمجيات"
+        bot.sendMessage(chat_id, a, reply_markup=json_keyboard)
+        conn = db.create_connection(database)
+        with conn:
+            db.insert_date(conn, 'bot', "send, mass, type,user,id,time",
+                           "'{}','{}','{}','{}', '{}','{}'".format(Suggestion, a, Suggestion, chat_username, chat_id,
+                                                                   tim))
+    elif command == "Open Source" or command == "اوبن سورس" or command == "المصادر المفتوحة" or command == "تقنيات المصادر المفتوحة" or command == "547" or command == "IT 547" or command == "Open Source Technology" :
+        a = "حدد المصدر الذي تريده"\
+            "توصيف المصادر المفتوحة"\
+            "المرجع الخاص بالمصادر المفتوحة"\
+            "الشرائح الخاصة بالمصادر المفتوحة"
         bot.sendMessage(chat_id, a, reply_markup=json_keyboard)
         conn = db.create_connection(database)
         with conn:
@@ -109,7 +122,7 @@ def handle(msg):
                            "'{}','{}','{}','{}', '{}','{}'".format(Suggestion, a, Suggestion, chat_username, chat_id,
                                                                    tim))
 
-    elif command == "Open Source" or command == "اوبن سورس" or command == "المصادر المفتوحة" or command == "المصادر":
+    elif command == "توصيف المصادر المفتوحة" or command == "توصيف اوبن سورس" or command == "توصيف مقرر المصادر المفتوحة":
         a = "توصيف المقرر: يهدف المقرر إلى تعريف الطالبة بتاريخ المصادر المفتوحه والنظام الإيكولوجي (ecosystem)، " \
             "وتزويدها بالمفاهيم المتعلقة بتراخيص البرمجيات الحرة والمصادر المفتوحه، وتكييف برمجيات المصادر المفتوحة " \
             "،وكذلك أساليب إنتاج برمجيات المصادر المفتوحة، بالإضافة إلى أدوات وتقنيات المصادر المفتوحة والتوجه " \
@@ -124,19 +137,82 @@ def handle(msg):
             db.insert_date(conn, 'bot', "send, mass, type,user,id,time",
                            "'{}','{}','{}','{}', '{}','{}'".format(Suggestion, a, Suggestion, chat_username, chat_id,
                                                                    tim))
-
-    elif command == "مادة" or command == "كورس":
-        a = " " \
-            "المشكلة "
+            
+    elif command == "المرجع الخاص بالمصادر المفتوحة" or command == "كتاب الاوبن سورس" or command == "كتاب مقرر المصادر المفتوحة":
+        a = "تفضل رابط التحميل" \
+            "http://www.mediafire.com/file/e8rmk9go5enyhfb/Books%20Open%20Source.zip"\
+            ""
         bot.sendMessage(chat_id, a, reply_markup=json_keyboard)
         conn = db.create_connection(database)
         with conn:
             db.insert_date(conn, 'bot', "send, mass, type,user,id,time",
                            "'{}','{}','{}','{}', '{}','{}'".format(Suggestion, a, Suggestion, chat_username, chat_id,
                                                                    tim))
-
+    
+    elif command == "الشرائح الخاصة بالمصادر المفتوحة" or command == "الفصول الخاصة بالاوبن سورس" or command == "اوبن سورس تشابترز":
+        a = "تفضل رابط التحميل" \
+            "http://www.mediafire.com/file/l62soykp2dsg890/Chapters%20Open%20source.zip"\
+            ""
+        bot.sendMessage(chat_id, a, reply_markup=json_keyboard)
+        conn = db.create_connection(database)
+        with conn:
+            db.insert_date(conn, 'bot', "send, mass, type,user,id,time",
+                           "'{}','{}','{}','{}', '{}','{}'".format(Suggestion, a, Suggestion, chat_username, chat_id,
+                                                                   tim))
+    
+    elif command == "software engineer" or command == "سوفتوير" or command == "هندسة" or command == "هندسة البرمجيات" or command == "323" or command == "IT 323" or command == "Software Engineer" :
+        a = "حدد المصدر الذي تريده"\
+            "توصيف هندسة البرمجيات"\
+            "المرجع الخاص بهندسة البرمجيات"\
+            "الشرائح الخاصة بهندسة البرمجيات"
+        bot.sendMessage(chat_id, a, reply_markup=json_keyboard)
+        conn = db.create_connection(database)
+        with conn:
+            db.insert_date(conn, 'bot', "send, mass, type,user,id,time",
+                           "'{}','{}','{}','{}', '{}','{}'".format(Suggestion, a, Suggestion, chat_username, chat_id,
+                                                                   tim))
+            
+    elif command == "توصيف هندسة البرمجيات" or command == "توصيف سوفتوير" or command == "توصيف مقرر هندسة البرمجيات":
+        a = "يتبع المقرر مقرر هندسة البرامج-1 حيث أنه يستمر في دراسة دورة حياة تطوير البرامج"\ 
+            "ستتمكن الطالبات من معاينة والتمكن من المراحل المتبقية من دورة حياة تطوير البرامج. "\
+            "بدءا من لمحة مفصلة عن علميات البرمجة ومراجعة ما قد تم تعلمه في المقرر السابق ، "\
+            "ستتعرض الطالبات لتغطية متعمقةلمفاهيم التصميم والتي لم يتم تقديمها في هندسة البرامج I،"\
+            "بما في ذلك التصميم المعماري ومباديء تصميم الواجهة. بعد ذلك سيتعرض المقرر لطرق مختلفة لتطوير البرامج،"\
+            "بما في ذلك التطوير السريع للبرامج، البرمجة المفرطة، وهندسة البرامج المعتمدة على العناصر."\
+            "كما يقدم المقرر بعد ذلك فاعلية البرامج والتحقق منها بما في ذلك عمليات التفتيش ومختلف تقنيات الاختبار الديناميكي."\
+            "بعد وضع النظم في الاستخدام لابد من تغييرها إذا كان عليها البقاء صالحة."\
+            "يعرض المقرر للطالبات عملية تطوير البرنامج و صيانة البرمجيات. تعتبر "\
+            "إدارة الكود وعملية التوثيق للنظام المطور من أهم العناصر في أي تطوير برمجي، يقدم المقرر للطالبات مبادئ إدارة التغيير."\ 
+            "وفي التهاية يعطي القرر لمحة موجزة عن إدارة جودة البرمجيات بما فيها مقاييس وقياسات البرامج" 
+        bot.sendMessage(chat_id, a, reply_markup=json_keyboard)
+        conn = db.create_connection(database)
+        with conn:
+            db.insert_date(conn, 'bot', "send, mass, type,user,id,time",
+                           "'{}','{}','{}','{}', '{}','{}'".format(Suggestion, a, Suggestion, chat_username, chat_id,
+                                                                   tim))
+    elif command == "المرجع الخاص بهندسة البرمجيات" or command == "كتاب السوفتوير" or command == "كتاب مقرر هندسة البرمجيات":
+        a = "تفضل رابط التحميل" \
+            "http://www.mediafire.com/file/fqk3cbtbg9ah3sx/Book.pdf"\
+            ""
+        bot.sendMessage(chat_id, a, reply_markup=json_keyboard)
+        conn = db.create_connection(database)
+        with conn:
+            db.insert_date(conn, 'bot', "send, mass, type,user,id,time",
+                           "'{}','{}','{}','{}', '{}','{}'".format(Suggestion, a, Suggestion, chat_username, chat_id,
+                                                                   tim))
+    
+    elif command == "الشرائح الخاصة بهندسة البرمجيات" or command == "الفصول الخاصة بهندسة البرمجيات" or command == "سوفتوير تشابترز":
+        a = "تفضل رابط التحميل" \
+            "http://www.mediafire.com/file/5v7l3l5vjsyujzj/Chapters%20Software%20Engineer.zip"\
+            ""
+        bot.sendMessage(chat_id, a, reply_markup=json_keyboard)
+        conn = db.create_connection(database)
+        with conn:
+            db.insert_date(conn, 'bot', "send, mass, type,user,id,time",
+                           "'{}','{}','{}','{}', '{}','{}'".format(Suggestion, a, Suggestion, chat_username, chat_id,
+                                                                   tim))
     else:
-        bot.sendMessage(chat_id, 'لم اعرف', reply_markup=json_keyboard)
+        bot.sendMessage(chat_id, ' لم اعرف ماذا تريد، أكتب "وجهني" أو "ماهذا البوت" حتى أستطيع مساعدتك', reply_markup=json_keyboard)
 
 
 MessageLoop(bot, handle).run_as_thread()
